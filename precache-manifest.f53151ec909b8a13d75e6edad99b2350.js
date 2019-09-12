6bb589c9c5595fe0cd297781a90b1a3ff8792684
Asick Ahamed <ashiq.it@gmail.com>
2019-09-12 17:00:04 +0200

    Updates

---
+++ precache-manifest.f53151ec909b8a13d75e6edad99b2350.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ef5d3ce257fb5424027de426b07a84d",
+    "url": "/medwing/index.html"
+  },
+  {
+    "revision": "75d6022a543ca75727d0",
+    "url": "/medwing/static/css/main.ad073390.chunk.css"
+  },
+  {
+    "revision": "b94b4ca90832e341408d",
+    "url": "/medwing/static/js/2.e717db85.chunk.js"
+  },
+  {
+    "revision": "75d6022a543ca75727d0",
+    "url": "/medwing/static/js/main.a44d4dba.chunk.js"
+  },
+  {
+    "revision": "4c68c7fd6da21300f538",
+    "url": "/medwing/static/js/runtime~main.7c35e480.js"
+  }
+]);
